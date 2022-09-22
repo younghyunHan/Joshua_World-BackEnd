90bc3896158041ce7f22eabf06df005776264504
younghyunHan <dudgus1670@gmail.com>
2022-09-22 09:34:58 +0900

    Fix: API 정리

--- app.js
+++ app.js
@@ -39,20 +39,57 @@ app.get("/", (req, res) => {
   res.render("index.html");
 });
 
-// app.get('/list', (req, res) => {
-//   connection.query('SELECT * from board', function (error, results, fields) {
-//     if (error) throw error;
-//     res.render('list', { data: results });
-//   });
-// });
+app.post("/signUp", (req, res) => {
+  // connection.connect(function (err) {
+  //  connection.connect() : DB접속
+  // if (err) throw err;
+  // console.log('Connected!');
+  connection.query(
+    {
+      sql: `INSERT INTO userInfo (user_id, user_pw, user_name) VALUES(?, MD5(?), ?)`,
+    },
+    [`${req.body.user_id}`, `${req.body.user_pw}`, `${req.body.user_name}`],
+    function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      res.send({ message: "SUCCESS" });
+    }
+  );
+  // });
+});
 
-// app.get('/api/list', (req, res) => {
-//   connection.query('SELECT * from board', function (error, results, fields) {
-//     if (error) throw error;
-//     res.send(results);
-//     // res.render('list', { data: results });
-//   });
-// });
+app.post("/signIn", (req, res) => {
+  // connection.connect(function (err) {
+  //  connection.connect() : DB접속
+  // if (err) throw err;
+  // console.log('Connected!');
+
+  connection.query(
+    { sql: `SELECT id FROM userInfo where user_id=? AND user_pw=MD5(?)` },
+    [req.body.user_id, req.body.user_pw],
+    function (err, result) {
+      if (err) throw err;
+      if (result.length !== 0) {
+        let token = jwt.sign(
+          {
+            id: result[0].id, // payload, private claims : 로그인 정보
+          },
+          "secretkey", // 비밀키(서명을 만들 때 사용되는 암호 문자열) -> signature
+          {
+            subject: "younghyun login jwtToken",
+            expiresIn: "60m",
+            issuer: "younghyun",
+          } // JWT생성할 때 사용되는 옵션. registered claims
+        );
+        console.log("토큰생성\n", token);
+        res.send({ token: token });
+      } else {
+        res.send({ message: "INVALID_USER" });
+      }
+    }
+  );
+  // });
+});
 
 app.get("/list", (req, res) => {
   const verify = jwt.verify(req.headers.authorization, "secretkey");
@@ -64,25 +101,6 @@ app.get("/list", (req, res) => {
       res.send(results);
     }
   );
-});
-
-// app.get('/category', (req, res) => {
-//   const verify = jwt.verify(req.headers.authorization, 'secretkey');
-//   connection.query(
-//     {
-//       sql: `SELECT * FROM board left JOIN category ON board.product_id = category.product_id where board.writer=?`,
-//     },
-//     [`${verify.id}`],
-//     function (error, results, fields) {
-//       if (error) throw error;
-//       res.send(results);
-//     }
-//   );
-// });
-const upload = multer({ dest: "uploads/" });
-app.post("/userInfoUpdate", upload.single("image"), (req, res) => {
-  console.log(req.file);
-  console.log(req.body);
 });
 
 app.get("/category", (req, res) => {
@@ -135,6 +153,12 @@ app.get("/searchData", (req, res) => {
   );
 });
 
+const upload = multer({ dest: "uploads/" });
+app.post("/userInfoUpdate", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+});
+
 app.post("/post", (req, res) => {
   // connection.connect(function (err) {
   // // connection.connect() : DB접속
@@ -155,58 +179,6 @@ app.post("/post", (req, res) => {
       if (err) throw err;
       console.log("1 record inserted");
       res.send({ message: "SUCCESS" });
-    }
-  );
-  // });
-});
-
-app.post("/signUp", (req, res) => {
-  // connection.connect(function (err) {
-  //  connection.connect() : DB접속
-  // if (err) throw err;
-  // console.log('Connected!');
-  connection.query(
-    {
-      sql: `INSERT INTO userInfo (user_id, user_pw, user_name) VALUES(?, MD5(?), ?)`,
-    },
-    [`${req.body.user_id}`, `${req.body.user_pw}`, `${req.body.user_name}`],
-    function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-      res.send({ message: "SUCCESS" });
-    }
-  );
-  // });
-});
-
-app.post("/signIn", (req, res) => {
-  // connection.connect(function (err) {
-  //  connection.connect() : DB접속
-  // if (err) throw err;
-  // console.log('Connected!');
-
-  connection.query(
-    { sql: `SELECT id FROM userInfo where user_id=? AND user_pw=MD5(?)` },
-    [req.body.user_id, req.body.user_pw],
-    function (err, result) {
-      if (err) throw err;
-      if (result.length !== 0) {
-        let token = jwt.sign(
-          {
-            id: result[0].id, // payload, private claims : 로그인 정보
-          },
-          "secretkey", // 비밀키(서명을 만들 때 사용되는 암호 문자열) -> signature
-          {
-            subject: "younghyun login jwtToken",
-            expiresIn: "60m",
-            issuer: "younghyun",
-          } // JWT생성할 때 사용되는 옵션. registered claims
-        );
-        console.log("토큰생성\n", token);
-        res.send({ token: token });
-      } else {
-        res.send({ message: "INVALID_USER" });
-      }
     }
   );
   // });
