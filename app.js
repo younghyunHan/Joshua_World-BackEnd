db33dcf6a6cdcaa1488f6c5ad3cadbc6f2bfda7e
younghyunHan <dudgus1670@gmail.com>
2022-09-23 14:28:27 +0900

    Fix: FormData 받기 구현 중

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 const mysql = require("mysql"); // mysql 모듈 로드
 
 var jwt = require("jsonwebtoken");
 
-const port = 3000;
 const cors = require("cors");
 const { resolveInclude } = require("ejs");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const conn = {
   // mysql 접속 설정
@@ -148,8 +149,34 @@ app.get("/searchData", (req, res) => {
   );
 });
 
+const upload = multer({
+  // storage : 어디에 저장할 것인지
+  // 서버 디스크에 저장하거나 AWS S3와 같은 외부에 저장합니다.
+  // multer-s3나 multer-google-storage와 같은 모듈을 찾아서 활용해봅시다
+  storage: multer.diskStorage({
+    //destination은 저장할 경로. 동일 경로 내 uploads에 저장할 것임.
+    // uploads 폴더를 생성해 둘 것.
+    destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    // filename은 저장할 파일의 이름
+    filename(req, file, cb) {
+      // ext는 확장자 명을 말합니다.
+      const ext = path.extname(file.originalname);
+      // basename은 파일 이름입니다. 파일 이름 + 현재 시간 + 확장자로 정하겠습니다.
+      // 날짜를 붙이는 건 중복을 피하기 위함입니다.
+      cb(
+        null,
+        path.basename(file.originalname, ext) + new Date().valueOf() + ext
+      );
+    },
+  }),
+  // limit : 파일 사이즈 제한 (byte 단위) 아래는 5mb 까지만 허용함을 의미
+  // limit: { fileSize: 5 * 1024 * 2014 },
+});
+
 app.post("/userInfoUpdate", upload.single("image"), (req, res) => {
-  console.log(req.header);
+  console.log(req.file);
   console.log(req.body);
 });
 
