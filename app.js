f937c67e961ff84dbb2f8f56fac5693a2ee2c6e5
younghyunHan <atuniverse@naver.com>
2022-12-19 14:06:44 +0900

    Fix : post API 500 Error 해결 중

--- app.js
+++ app.js
@@ -165,58 +165,32 @@ app.post("/signIn", (req, res) => {
 //   );
 // });
 
-const upload = multer({
-  // storage : 어디에 저장할 것인지
-  // 서버 디스크에 저장하거나 AWS S3와 같은 외부에 저장.
-  // multer-s3나 multer-google-storage와 같은 모듈을 찾아서 활용.
-  storage: multer.diskStorage({
-    // destination은 저장할 경로. 동일 경로 내 uploads에 저장할 것임.
-    // uploads 폴더를 생성해 둘 것.
-    destination(req, file, cb) {
-      cb(null, "uploads/");
-    },
-    // filename은 저장할 파일의 이름
-    filename(req, file, cb) {
-      // ext는 확장자 명을 말함.
-      const ext = path.extname(file.originalname);
-      // basename은 파일 이름. 파일 이름 + 현재 시간 + 확장자로 정함.
-      // 날짜를 붙이는 건 중복을 피하기 위함.
-      cb(
-        null,
-        path.basename(file.originalname, ext) + new Date().valueOf() + ext
-      );
-    },
-  }),
-  // limit : 파일 사이즈 제한 (byte 단위) 아래는 5mb 까지만 허용함을 의미
-  // limit: { fileSize: 5 * 1024 * 2014 },
-});
+// app.post("/userInfoUpdate", upload.single("user_img"), (req, res) => {
+//   const verify = jwt.verify(req.headers.authorization, "secretkey");
+//   console.log(verify);
+//   // console.log(req.file); // 이미지 파일
+//   // console.log(req.body); // 닉네임, 비밀번호
 
-app.post("/userInfoUpdate", upload.single("user_img"), (req, res) => {
-  const verify = jwt.verify(req.headers.authorization, "secretkey");
-  console.log(verify);
-  // console.log(req.file); // 이미지 파일
-  // console.log(req.body); // 닉네임, 비밀번호
-
-  connection.query(
-    {
-      sql: `UPDATE userInfo SET user_name=?, user_pw=MD5(?), user_img=? where userInfo.id=?`,
-    },
-    [
-      `${req.body.user_name}`,
-      `${req.body.user_pw}`,
-      `${req.file.path}`,
-      `${verify.id}`,
-    ],
-    function (error, results, fields) {
-      if (error) throw error;
-      res.send({
-        message: "SUCCESS",
-        user_img: req.file,
-        user_name: req.body.user_name,
-      });
-    }
-  );
-});
+//   connection.query(
+//     {
+//       sql: `UPDATE userInfo SET user_name=?, user_pw=MD5(?), user_img=? where userInfo.id=?`,
+//     },
+//     [
+//       `${req.body.user_name}`,
+//       `${req.body.user_pw}`,
+//       `${req.file.path}`,
+//       `${verify.id}`,
+//     ],
+//     function (error, results, fields) {
+//       if (error) throw error;
+//       res.send({
+//         message: "SUCCESS",
+//         user_img: req.file,
+//         user_name: req.body.user_name,
+//       });
+//     }
+//   );
+// });
 
 // app.post("/postUpdate", (req, res) => {
 //   // connection.connect(function (err) {
@@ -243,32 +217,92 @@ app.post("/userInfoUpdate", upload.single("user_img"), (req, res) => {
 //   // });
 // });
 
-app.post("/post", (req, res) => {
-  // connection.connect(function (err) {
-  // // connection.connect() : DB접속
-  // if (err) throw err;
-  // console.log('Connected!');
-  const verify = jwt.verify(req.headers.authorization, "secretkey");
-  connection.query(
-    {
-      sql: `INSERT INTO board (postTitle, postThumbnailLink, postThumbnailImg, postMainHtml) VALUES (?, ?, ?, ?)`,
+const upload = multer({
+  // storage : 어디에 저장할 것인지
+  // 서버 디스크에 저장하거나 AWS S3와 같은 외부에 저장.
+  // multer-s3나 multer-google-storage와 같은 모듈을 찾아서 활용.
+  storage: multer.diskStorage({
+    // destination은 저장할 경로. 동일 경로 내 uploads에 저장할 것임.
+    // uploads 폴더를 생성해 둘 것.
+    destination(req, file, cb) {
+      cb(null, "uploads/");
     },
-    [
-      `${verify.id}`,
-      `${req.body.content.postTitle}`,
-      `${req.body.content.postThumbnailLink}`,
-      `${req.body.content.postThumbnailImg}`,
-      `${req.body.content.postMainHtml}`,
-    ],
-
-    function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-      res.send({ message: "SUCCESS" });
-    }
-  );
-  // });
+    // filename은 저장할 파일의 이름
+    filename(req, file, cb) {
+      // ext는 확장자 명을 말함.
+      const ext = path.extname(file.originalname);
+      // basename은 파일 이름. 파일 이름 + 현재 시간 + 확장자로 정함.
+      // 날짜를 붙이는 건 중복을 피하기 위함.
+      cb(
+        null,
+        path.basename(file.originalname, ext) + new Date().valueOf() + ext
+      );
+    },
+  }),
+  // limit : 파일 사이즈 제한 (byte 단위) 아래는 5mb 까지만 허용함을 의미
+  // limit: { fileSize: 5 * 1024 * 2014 },
 });
+
+app.post("/post", upload.single("postThumbnailImg"), (req, res) => {
+  const verify = jwt.verify(req.headers.authorization, "secretkey");
+  console.log(verify);
+  console.log(req);
+
+  // console.log(req.file); // 이미지 파일
+  // console.log(req.body); // postTitle, postThumbnailLink, postMainHtml
+
+  // connection.query(
+  //   {
+  //     sql: `INSERT INTO post (postTitle, postThumbnailLink, postThumbnailImg, postMainHtml) VALUES (?, ?, ?, ?)`,
+  //   },
+  //   [
+  //     `${req.body.user_name}`,
+  //     `${req.body.user_pw}`,
+  //     `${req.file.path}`,
+  //     `${verify.id}`,
+  //   ],
+  //   function (error, results, fields) {
+  //     if (error) throw error;
+  //     res.send({
+  //       message: "SUCCESS",
+  //       user_img: req.file,
+  //       user_name: req.body.user_name,
+  //     });
+  //   }
+  // );
+});
+
+// app.post("/post", (req, res) => {
+//   // connection.connect(function (err) {
+//   // connection.connect() : DB접속
+//   // if (err) throw err;
+//   // console.log("Connected!");
+
+//   const verify = jwt.verify(req.headers.authorization, "secretkey");
+//   connection.query(
+//     {
+//       sql: `INSERT INTO board (postTitle, postThumbnailLink, postThumbnailImg, postMainHtml) VALUES (?, ?, ?, ?)`,
+//     },
+//     [
+//       `${verify.id}`,
+//       `${req.body.content.postTitle}`,
+//       `${req.body.content.postThumbnailLink}`,
+//       `${req.body.content.postThumbnailImg}`,
+//       `${req.body.content.postMainHtml}`,
+//     ],
+
+//     function (err, result) {
+//       if (err) throw err;
+//       console.log("1 record inserted");
+//       res.send({
+//         message: "SUCCESS",
+//         user_img: req.file,
+//         user_name: req.body.user_name,
+//       });
+//     }
+//   );
+//   // });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
